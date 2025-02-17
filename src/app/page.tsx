f4e78b752c03f18d8d6a6367edc0c42c0389a06f
abdroos <abd.alaidrous@gmail.com>
2025-02-17 12:04:03 +0300

    refactor: update font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,16 +26,16 @@ export default function Home() {
             <div className="container mx-auto">
               <Tabs defaultValue="home" className="w-full">
                 <TabsList dir="rtl" className="w-full p-2 grid grid-cols-2 sm:grid-cols-3 md:flex gap-2 md:gap-4 h-auto">
-                  <TabsTrigger className="text-sm sm:text-base md:text-lg lg:text-xl px-3 py-2 sm:px-4 sm:py-2 md:px-6 md:py-3 lg:px-8 lg:py-3 min-w-[100px] text-center" value="home">
+                  <TabsTrigger className="text-base md:text-lg lg:text-xl px-3 py-2 sm:px-4 sm:py-2 md:px-6 md:py-3 lg:px-8 lg:py-3 min-w-[100px] text-center" value="home">
                     {t('nav.home')}
                   </TabsTrigger>
-                  <TabsTrigger className="text-sm sm:text-base md:text-lg lg:text-xl px-3 py-2 sm:px-4 sm:py-2 md:px-6 md:py-3 lg:px-8 lg:py-3 min-w-[100px] text-center" value="accommodation">
+                  <TabsTrigger className="text-base md:text-lg lg:text-xl px-3 py-2 sm:px-4 sm:py-2 md:px-6 md:py-3 lg:px-8 lg:py-3 min-w-[100px] text-center" value="accommodation">
                     {t('nav.accommodation')}
                   </TabsTrigger>
-                  <TabsTrigger className="text-sm sm:text-base md:text-lg lg:text-xl px-3 py-2 sm:px-4 sm:py-2 md:px-6 md:py-3 lg:px-8 lg:py-3 min-w-[100px] text-center" value="location">
+                  <TabsTrigger className="text-base md:text-lg lg:text-xl px-3 py-2 sm:px-4 sm:py-2 md:px-6 md:py-3 lg:px-8 lg:py-3 min-w-[100px] text-center" value="location">
                     {t('nav.location')}
                   </TabsTrigger>
-                  <TabsTrigger className="text-sm sm:text-base md:text-lg lg:text-xl px-3 py-2 sm:px-4 sm:py-2 md:px-6 md:py-3 lg:px-8 lg:py-3 min-w-[100px] text-center" value="salons">
+                  <TabsTrigger className="text-base md:text-lg lg:text-xl px-3 py-2 sm:px-4 sm:py-2 md:px-6 md:py-3 lg:px-8 lg:py-3 min-w-[100px] text-center" value="salons">
                     {t('nav.salons')}
                   </TabsTrigger>
                 </TabsList>
